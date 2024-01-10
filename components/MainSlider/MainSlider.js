ecd92faade8a396ac41af40ff4f8e0f776d3de51
Harshi Agrawal <114180714+harshi706@users.noreply.github.com>
2024-01-10 15:21:54 +0530

    Mainslider

--- components/MainSlider/MainSlider.js
+++ components/MainSlider/MainSlider.js
@@ -1,215 +1,131 @@
-"use client";
-import React, { useRef, useState, useEffect } from "react";
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
-// import { Oval } from "react-loader-spinner";
-import "./Mainslidestyle.css";
+import React from "react";
 import { list3 } from "./mainslide-list";
-// import _debounce from "lodash/debounce";
-import { useDispatch, useSelector } from "react-redux";
-import { selectSliderData } from "../Features/Slices/sliderSlice";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./styles.css";
+import { Autoplay, Navigation } from "swiper/modules";
+import SwiperCore from "swiper/core";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+SwiperCore.use([Autoplay, Navigation]);
 
-function MainSlider() {
-  //data fetching
-  const dispatch = useDispatch();
-  const SliderViewData = useSelector(selectSliderData);
-  const [page, setPage] = useState(1);
-  useEffect(() => {
-    if (!SliderViewData || SliderViewData.length === 0) {
-      fetchData();
-    }
-  }, [page]);
-  const fetchData = () => {
-    dispatch({
-      type: "FETCH_SLIDER_VIEW_REQUEST",
-      payload: {
-        page: page,
-        limit: 3,
-      },
-    });
+export default function MainSlider() {
+  const router = useRouter();
+  const handleTab = () => {
+    router.push("/room");
   };
-  console.log(SliderViewData);
-  const [sliderApiData, setSliderApiData] = useState([]);
-  useEffect(() => {
-    if (SliderViewData && SliderViewData.result) {
-      setSliderApiData(SliderViewData.result);
-    }
-  }, [SliderViewData]);
-  const products = list3.filter(
-    (prod) => prod.id === 1 || prod.id === 2 || prod.id === 3
-  );
-  const [scrollX, setScrollX] = useState(0);
-  const scrl = useRef(null);
-
-  useEffect(() => {
-    if (scrl.current) {
-      scrl.current.scrollLeft = 0;
-      scrl.current.style.transition = "transition 4s";
-    }
-  }, []);
-  const slideswipe = window.innerWidth / 3;
-
-  const slide = (shift) => {
-    if (scrl.current) {
-      let targetScroll = scrl.current.scrollLeft + shift;
-
-      // scrl.current.scrollIntoView({ behavior: 'smooth' });
-      const totalWidth = scrl.current.scrollWidth;
-      const containerWidth = scrl.current.clientWidth;
-      const children = scrl.current.children;
-      const firstChild = children[0];
-      const lastChild = children[children.length - 1];
-
-      if (targetScroll < 0) {
-        // If scrolling left past the start, move the last child to the beginning
-        scrl.current.insertBefore(lastChild, firstChild);
-        targetScroll += lastChild.offsetWidth;
-      } else if (targetScroll + containerWidth > totalWidth) {
-        // If scrolling right past the end, move the first child to the end
-        scrl.current.appendChild(firstChild);
-        targetScroll -= firstChild.offsetWidth;
-      }
-      // scrl.current.style.transform = "translateX(-50%)"
-      // scrl.current.style.transition = "all 3s";
-      scrl.current.scrollTo({
-        left: targetScroll,
-        behavior: "smooth",
-      });
-
-      setScrollX(targetScroll);
-    }
-  };
-
-  // **********************************
-  // for mobile
-
-  const [touchStart, setTouchStart] = useState(0);
-  const [touchEnd, setTouchEnd] = useState(0);
-
-  const handleTouchStart = (e) => {
-    setTouchStart(e.targetTouches[0].clientX);
-  };
-
-  const handleTouchMove = (e) => {
-    setTouchEnd(e.targetTouches[0].clientX);
-  };
-
-  const handleTouchEnd = () => {
-    if (touchStart - touchEnd > slideswipe) {
-      // the user swiped left
-      const firstChild = scrl.current.firstChild;
-      const clone = firstChild.cloneNode(true);
-      scrl.current.appendChild(clone);
-      scrl.current.removeChild(firstChild);
-      scrl.current.scrollTo({
-        left: 0,
-        behavior: "smooth",
-      });
-    }
-
-    if (touchStart - touchEnd < slideswipe) {
-      // the user swiped right
-      const lastChild = scrl.current.lastChild;
-      const clone = lastChild.cloneNode(true);
-      scrl.current.insertBefore(clone, scrl.current.firstChild);
-      scrl.current.removeChild(lastChild);
-      scrl.current.scrollTo({
-        left: scrl.current.scrollWidth - scrl.current.clientWidth,
-        behavior: "smooth",
-      });
-    }
-  };
-
-  useEffect(() => {
-    const slideInterval = setInterval(() => {
-      slide(slideswipe);
-    }, 4000);
-    return () => {
-      clearInterval(slideInterval);
-    };
-  }, [slideswipe]);
-
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 450);
-  useEffect(() => {
-    // Update isMobile state on window resize
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 450);
-    };
-    window.addEventListener("resize", handleResize);
-    // Cleanup event listener on component unmount
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
+  const circled = [
+    {
+      top: 50,
+      left: 68,
+      productTitle: "Bedroom 1bhk Room",
+      productCategory: "Mountain view",
+      price: 1900,
+    },
+  ];
+  const content=[
+    '/map1.jpg',    
+    '/map1.jpg',
+    '/map1.jpg',
+    '/map1.jpg',
+    '/map1.jpg',
+    '/map1.jpg',
+  ]
   return (
-    <div className="slider-container">
-      <div className="arrow-left z-20" onClick={() => slide(-slideswipe)}>
-        {!isMobile && scrollX > 0 && (
-          <BsArrowLeftCircleFill className="arrow-nav" />
-        )}
-      </div>
-      <div
-        className="slider-cont"
-        id="my-slider-container"
-        ref={scrl}
-        onTouchStart={handleTouchStart}
-        onTouchMove={handleTouchMove}
-        onTouchEnd={handleTouchEnd}
+    <div>
+      <Swiper
+        className="swiper-slider"
+        centeredSlides={true}
+        slidesPerView={1}
+        grabCursor={true}
+        freeMode={false}
+        loop={true}
+        mousewheel={false}
+        keyboard={{
+          enabled: true,
+        }}
+        // Enabled autoplay mode
+        autoplay={{
+          delay: 10000,
+          disableOnInteraction: false,
+        }}
+        // If we need pagination
+        pagination={{
+          el: ".swiper-pagination",
+          dynamicBullets: false,
+          clickable: true,
+        }}
+        // If we need navigation
+        navigation={{
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        }}
+        // Responsive breakpoints
+        breakpoints={{
+          412: {
+            slidesPerView: 1.5,
+            spaceBetween: 15,
+          },
+
+          640: {
+            slidesPerView: 1.25,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+        }}
       >
-        {sliderApiData.length > 0 &&
-          sliderApiData.map((prod, i) => (
-            <div key={i}>
-              <div className="circle-container relative items-center justify-center flex sd">
-                  <img
-                    className="sd rounded-sm"
-                    src={prod.imgSrc}
-                    alt="Product"
-                  />
-                {prod.circles.map((circle, index) => (
+    {content.map((data) => (
+  <SwiperSlide key={data}>
+    <div className="relative group">
+      <Image src={data} width={500} height={500} alt="Swiper" className="swiper-slide" />
+      <div className="absolute bottom-4 left-4 flex text-lg text-white">Your text</div>
+      <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+        <div className="text-md text-black absolute top-44 left-52 bg-gray-500 rounded-full w-5 h-5">
+          <div className="bg-white h-3 w-3 absolute rounded-full ml-1 mt-1 main">
+            <div className="hidden opacity-0 main-hover:opacity-100"></div>
+            <div
+              className={`flex-row z-10 mt-4 w-40 h-44 flex items-center pb-2 bg-white cursor-pointer`}
+              onClick={handleTab}
+            >
+              <div className="flex flex-row relative">
+                {list3.map((data) => (
                   <div
-                    key={index}
-                    className={`circle absolute effect`}
-                    style={{
-                      top: `${circle.topPosition}%`,
-                      left: `${circle.leftPosition}%`,
-                    }}
+                    className="flex flex-col basis-3/4 w-36 flex-grow relative ml-1 mr-2.5 pr-4"
+                    key={data.productId}
                   >
-                    <div
-                      className={`hover-box flex-row z-10 w-44 h-44 flex items-center bg-white
-                     ${circle.topPosition > 75 ? "top-condition" : ""} ${
-                        circle.leftPosition > 65 ? "left-condition" : ""
-                      }
-                    `}
-                    >
-                      <div className="flex flex-col">
-                        <div className="flex flex-col basis-3/4 w-36 flex-grow relative -ml-1">
-                          <h2 className="font-bold pt-1 pr-2">
-                            {circle.productTitle}
-                          </h2>
-                          <p className="font-normal">
-                            {circle.productCategory}
-                          </p>
-                          <p className="font-bold bg-yellow-400 h-8 w-16 pl-2 main">
-                            ₹ {circle.productPrice}
-                          </p>
-                          <br />
-                        </div>
-                        <div className="absolute inset-y-0 right-0  border-l border-black flex items-center justify-center basis-1/4 flex-grow pl-2 pr-2 ml-2">
-                          <BsArrowRightCircleFill className="flex items-center justify-center" />
-                        </div>
-                      </div>
-                    </div>
+                    <h2 className="font-bold pt-1 pr-2">{data.circles[0].productTitle}</h2>
+                    <p className="font-normal pb-2">{data.circles[0].productCategory}</p>
+                    <p className="font-bold bg-yellow-400 h-8 w-16 pl-2 main">
+                      ₹{data.circles[0].price}
+                    </p>
                   </div>
                 ))}
+                <div className="absolute right-0 top-0 border-l border-black flex justify-end items-center h-full pr-1">
+                  <Image
+                    className="flex ml-2"
+                    src="/backarrowRevarce.svg"
+                    height={20}
+                    width={20}
+                    alt="arrow"
+                  />
+                </div>
               </div>
             </div>
-          ))}
-        {/* </div> */}
+          </div>
+        </div>
       </div>
-      <div className="arrow-rightS z-20" onClick={() => slide(+slideswipe)}>
-        {!isMobile && <BsArrowRightCircleFill className="arrow-nav" />}
-      </div>
+    </div>
+  </SwiperSlide>
+))}
+          <div className="swiper-pagination"></div>
+        {/* <div className="swiper-button-prev"></div> */}
+        <div className="swiper-button-next sm:-translate-y-[150px]"></div>
+      </Swiper>
     </div>
   );
 }
-export default MainSlider;
